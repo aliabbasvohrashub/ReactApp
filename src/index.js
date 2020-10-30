efaf486b486198ce5a3bc33a1a761e3c8c77aaf2
AliabbasVohra <aliabbasvohra@gmail.com>
2020-10-30 11:13:12 +0530

    first react app ready

--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import TodoContainer from "./components/TodoContainer";
+//import registerServiceWorker from './registerServiceWorker';
+
+//import App from './App'
+
+var element = React.createElement('h1', {className: 'greeting'},'Hello World!');
+
+//ReactDOM.render(element, document.getElementById('root'));
+
+////registerServiceWorker();
+
+ReactDOM.render(<TodoContainer></TodoContainer>, document.getElementById('root'));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
